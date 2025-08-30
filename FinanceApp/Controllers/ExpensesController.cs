5877ec120d89796f9c2dbef1a7c0c5f552812eac
Pritam Barua <pritambarua99@gmail.com>
2025-08-30 22:32:09 +0530

    Add project files.

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,46 @@
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+using FinanceApp.Data.Services;
+
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IExpensesService _expesesService;
+
+
+        public ExpensesController(IExpensesService expesesService)
+        {
+            _expesesService = expesesService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _expesesService.GetAll();
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                await _expesesService.Add(expense);
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+        
+        public IActionResult GetChart()
+        {
+            var data = _expesesService.GetChartData();
+            return Json(data);
+        }
+    }
+}
---
+++ FinanceApp/Data/Services/ExpensesService.cs
@@ -0,0 +1,37 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data.Services
+{
+    public class ExpensesService : IExpensesService
+    {
+        private readonly FinanceAppContext _context;
+
+        public ExpensesService(FinanceAppContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Expense>> GetAll()
+        {
+            var expenses = await _context.Expenses.ToListAsync();
+            return expenses;
+        }
+
+        public IQueryable GetChartData()
+        {
+            var data = _context.Expenses.GroupBy(e => e.Category).Select(s => new
+            {
+                Category = s.Key,
+                Total = s.Sum(e => e.Amount)
+            });
+            return data;
+
+        }
+    }
+}
